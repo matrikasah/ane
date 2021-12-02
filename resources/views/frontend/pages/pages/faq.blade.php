dd4aea1fc2022d1a4943096827efb1bcbfbfdc88
suminamaharjan12 <suminamaharjan12@gmail.com>
2021-12-02 12:00:40 +0545

    ane new

---
+++ resources/views/frontend/pages/pages/faq.blade.php
@@ -0,0 +1,172 @@
+@extends('frontend.master')
+@section('content')
+<section class="featured-img">
+	<img src="{{ asset('image/course-featured.jpg') }}">
+	<div class="overlap-featured-breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-8 col-lg-7">
+					<h1 class="innerpage-title">FAQs</h1>
+					<p>frequently asked questions</p>
+					<nav aria-label="breadcrumb">
+					  <ol class="breadcrumb">
+					    <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+					    <li class="breadcrumb-item active" aria-current="page">FAQs</li>
+					  </ol>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="innerpage-content">
+	<div class="container">
+		<div class="row content-text">
+			<div class="col-sm-5 col-lg-4">
+				<div class="ceo-img">
+					<img src="{{ asset('image/faq.jpg') }}" class="img-responsive">
+				</div>
+			</div>
+			<div class="col-sm-7 col-lg-7">
+				<div class="small-title title">
+						<h3>FAQs</h3>
+						<h1>frequently asked questions</h1>
+					</div>
+					
+
+					<div class="accordion accordion-flush content-text" id="accordionFlushExample">
+					  <div class="accordion-item">
+						    <h2 class="accordion-header" id="flush-headingOne">
+						      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+						        What is ANE College’s response to COVID-19?
+
+						      </button>
+						    </h2>
+						    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+						      <div class="accordion-body">
+						      
+						      		ANE College has a COVID-19 Protective Plan, which includes effective measures to protect students and staff members from COVID-19. The measures have been strictly implemented. This includes:
+						     
+						      	<ul>
+									<li><i class="fas fa-angle-double-right"></i>Social-distancing Rules</li>
+									<li><i class="fas fa-angle-double-right"></i>Zoom set-up classes</li>
+									<li><i class="fas fa-angle-double-right"></i>Facemasks and hand wash soap were emplaced</li>
+									<li><i class="fas fa-angle-double-right"></i>Aware and update students with helpful information regarding COVID-19</li>
+									<li><i class="fas fa-angle-double-right"></i>Restriction of face-to-face students in college</li>
+									<li><i class="fas fa-angle-double-right"></i>Minimum students in the class, around ten students per class.</li>
+									</ul>
+								</div>
+						    </div>
+					  </div>
+					  <div class="accordion-item">
+						    <h2 class="accordion-header" id="flush-headingTwo">
+						      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+						        Why should I choose ANE College over other study options?
+						      </button>
+						    </h2>
+						    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+						      <div class="accordion-body">
+						      	Over the years, we have developed an innovative approach to help students achieve their dream. Our course contents, classrooms, and highly experienced tutors are entirely different from any others. If you want to learn something quickly and efficiently, ANE College is right for you.
+								
+							</div>
+					    </div>
+					  </div>
+					  <div class="accordion-item">
+						    <h2 class="accordion-header" id="flush-headingThree">
+						      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+						        What kind of job could I get after graduating from ANE College?
+						      </button>
+						    </h2>
+						    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+						      <div class="accordion-body">
+						      	With employability at the centre of everything we do, we prepare you for a wide variety of careers.
+							</div>
+					    </div>
+					  </div>
+					  <div class="accordion-item">
+						    <h2 class="accordion-header" id="flush-headingFour">
+						      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+						         Do ANE College courses have a language requirement?
+						      </button>
+						    </h2>
+						    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+						      <div class="accordion-body">With employability at the centre, we prepare you for a wide variety of careers.
+								</div>
+						    </div>
+					  </div>
+					  <div class="accordion-item">
+						    <h2 class="accordion-header" id="flush-headingFive">
+						      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+						        Does ANE College help graduates get jobs?
+						      </button>
+						    </h2>
+						    <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+						      <div class="accordion-body">We encourage students to tackle job hunts independently, but we provide every required assistance along the way.
+
+
+
+								
+								</div>
+						    </div>
+					  </div>
+					</div>
+					 
+
+
+
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+
+<section class="gap enrollment">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<div class="card" data-aos="fade-up">
+				<div class="card-body">
+				  	<div class="left">
+				  		<img src="{{ asset('image/contact.svg') }}">
+				  	</div>
+				  	<div class="right">
+				  		<h5 class="card-title">Contact</h5>
+					    <p class="card-text">Feel free to contact us anytime you need.</p>
+				  	</div>
+				  </div>
+				  </div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<div class="card" data-aos="fade-up">
+				<div class="card-body">
+				  	<div class="left">
+				  		<img src="{{ asset('image/consult.svg') }}">
+				  	</div>
+				  	<div class="right">
+				  		<h5 class="card-title">Consult</h5>
+					    <p class="card-text">Dedicated staff are there to answer any questions.</p>
+				  	</div>
+				  </div>
+				  </div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+				<div class="card" data-aos="fade-up">
+				<div class="card-body">
+				  	<div class="left">
+				  		<img src="{{ asset('image/admission.svg') }}">
+				  	</div>
+				  	<div class="right">
+				  		<h5 class="card-title">Admission</h5>
+					    <p class="card-text">Apply in ANE College for your interested course right away.</p>
+				  	</div>
+				  </div>
+				  </div>
+			</div>
+		</div>
+	</div>
+</section>
+
+@endsection
