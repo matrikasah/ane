c014db9dea9a953a9b7941dbe98d3f320805fcbf
Bivek Pandey <pandeyvivak25@hotmail.com>
2021-12-09 11:20:56 +0545

    scripts for security

--- public/js/Script.js
+++ public/js/Script.js
@@ -182,3 +182,29 @@ $(".allow_decimal").on("input", function(evt) {
     evt.preventDefault();
   }
 });
+
+document.addEventListener('keydown', function() {
+  if (event.keyCode == 123) {
+    
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+   
+     event.preventDefault();
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    
+    return false;
+  }
+}, false);
+
+if (document.addEventListener) {
+  document.addEventListener('contextmenu', function(e) {
+    
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent('oncontextmenu', function() {
+    
+    window.event.returnValue = false;
+  });
+}
